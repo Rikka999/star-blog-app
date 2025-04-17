1f78b2563b3f1c3f7024165164990ccc1479840a
MC <evileyerikka@foxmail.com>
2025-04-18 01:10:59 +0800

    添加路由

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
-import TarotView from '../views/tarot/TarotView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import TarotView from '@/views/tarot/TarotView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/login', component: LoginView },
+  { path: '/register', component: RegisterView},
   { path: '/tarot', component: TarotView }
 ]
 
