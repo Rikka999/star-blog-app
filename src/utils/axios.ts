623b941bcbe60d8963b9630cb3f12ab5324a7072
MC <evileyerikka@foxmail.com>
2025-05-30 12:39:57 +0800

    token过期处理

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
+import { ElMessage } from 'element-plus';
+import { useUserStore } from '@/stores/user';
+import { router } from '@/router/router';
 
 const instance = axios.create({
   baseURL: 'http://192.168.1.11:8080',
   timeout: 5000
 });
+
+const userInfo = useUserStore();
 
 // 请求拦截器，添加 token
 instance.interceptors.request.use(
@@ -17,6 +22,23 @@ instance.interceptors.request.use(
     return config;
   },
   error => {
+    return Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.data?.status === 401) {
+      ElMessage.warning('登录已过期，请重新登录');
+      userInfo.clearUser();
+      router.push('/').then(() => {
+        location.reload();
+      });
+    } else {
+      ElMessage.error(error.response?.data?.message || '请求失败');
+    }
     return Promise.reject(error);
   }
 );
